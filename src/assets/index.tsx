552646834049140716337d7f402702b5f91aa0c0
AqibIqbal1 <aqib.dev021@gmail.com>
2025-04-24 22:55:39 +0500

    Add new icon assets for various UI elements
    - Added filled and outlined versions of the following icons:
      - Add
      - Dress
      - Gallery
      - Home
      - User
    
    These icons are in PNG format and are intended to enhance the visual representation of the application.

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,7 +1,30 @@
 // Icons
 const eyeIcon = require('./icons/eye.png');
 const eyeFilledIcon = require('./icons/eye-filled.png');
+const homeIcon = require('./icons/home.png');
+const homeFilledIcon = require('./icons/home-filled.png');
+const userIcon = require('./icons/user.png');
+const userFilledIcon = require('./icons/user-filled.png');
+const dressIcon = require('./icons/dress.png');
+const dressFilledIcon = require('./icons/dress-filled.png');
+const galleryIcon = require('./icons/gallery.png');
+const galleryFilledIcon = require('./icons/gallery-filled.png');
+const addIcon = require('./icons/add.png');
+const addFilledIcon = require('./icons/add-filled.png');
 
 // Images
 
-export {eyeIcon, eyeFilledIcon};
+export {
+  eyeIcon,
+  eyeFilledIcon,
+  homeIcon,
+  homeFilledIcon,
+  userIcon,
+  userFilledIcon,
+  dressIcon,
+  dressFilledIcon,
+  galleryIcon,
+  galleryFilledIcon,
+  addIcon,
+  addFilledIcon,
+};
